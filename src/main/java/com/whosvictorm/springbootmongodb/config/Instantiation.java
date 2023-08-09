3fad37edf6f35ecea2e96ff01b4bbe87a1242755
Victor <victormagalhaespb@gmail.com>
2023-08-09 14:22:31 -0300

    Add comments to posts

--- src/main/java/com/whosvictorm/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/whosvictorm/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.whosvictorm.springbootmongodb.config;
 import com.whosvictorm.springbootmongodb.domain.Post;
 import com.whosvictorm.springbootmongodb.domain.User;
 import com.whosvictorm.springbootmongodb.dto.AuthorDTO;
+import com.whosvictorm.springbootmongodb.dto.CommentDTO;
 import com.whosvictorm.springbootmongodb.repository.PostRepository;
 import com.whosvictorm.springbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null,sdf.parse("09/08/2023"), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null,sdf.parse("08/08/2023"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("09/08/2023"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("09/08/2023"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("08/08/2023"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
